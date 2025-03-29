dc1f07105842763aa2859353adb16ae0e1fa07ec
HanyRabah <hany.rabah@gmail.com>
2025-03-29 14:39:03 +0200

    fix hero video

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { Volume2, VolumeOff } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
 
 const Hero = () => {
@@ -16,33 +15,14 @@ const Hero = () => {
 		}
 	}, []);
 
-	const handleMuteToggle = () => {
-		if (videoRef.current) {
-			if (videoRef.current.muted) {
-				videoRef.current.muted = false;
-				setVideoState({ muted: false });
-			} else {
-				videoRef.current.muted = true;
-				setVideoState({ muted: true });
-			}
-		}
-	};
-
 	return (
 		<div className="relative w-full h-[40vh] md:h-[80vh]">
 			{/* Video Background */}
 			<video ref={videoRef} className="absolute inset-0 w-full h-full object-cover" autoPlay loop muted playsInline>
 				<source src="/videos/zero31.webm" type="video/webm" />
 			</video>
-			{/* Mute Button */}
-			<button
-				className="absolute bottom-6 right-6 z-20 p-4 bg-white rounded-full shadow-md"
-				onClick={handleMuteToggle}
-				aria-label="Mute/Unmute Video">
-				{videoState.muted ? <VolumeOff /> : <Volume2 />}
-			</button>
 			{/* Gradient Overlay */}
-			<div className="absolute inset-0 bg-gradient-to-b from-black/40 to-black/20" />
+			{/* <div className="absolute inset-0 bg-gradient-to-b from-black/40 to-black/20" /> */}
 			{/* Content */}
 			{/* <motion.div
 				className="absolute inset-0 z-10 flex items-center justify-center p-4"
